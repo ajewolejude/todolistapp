78887201eb577fc1f0ecbacfb3af08debe4e7b5e
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2018-10-25 16:45:17 +0100

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private goals = new BehaviorSubject<any>(['Drink Coffee','Start Computer']);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal){
+    this.goals.next(goal);
+  }
+}
